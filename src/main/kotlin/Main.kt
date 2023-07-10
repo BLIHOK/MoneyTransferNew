3b02bed9e8547a5d848e6d3bc788f0ea58dd66ba
Denis Aksenov <BLIHOK@mail.ru>
2023-07-10 21:28:35 +0300

    Add main coverage test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,34 @@
-fun main(args: Array<String>) {
-//    println("Введи сумму перевода")
+fun main() {
+
     val amount = 100
     val fee = 0.0075
     val minFee = 35
+
+    val resultFee = calculateFee(amount, fee, minFee)
+
+    val resultString =
+        println(
+            """
+        Сумма перевода: $amount
+        Размер комиссии составит: $resultFee
+    """.trimIndent()
+        )
+
+}
+
+
+fun onlyForTest(someInt: Int, someString: String): String {
+    val sumI = someInt * 2
+    val sumS = someString + someString
+    val concat = if (sumI > 5) "$sumI $sumS" else sumS
+    return concat
+}
+
+fun calculateFee(amount: Int, fee: Double, minFee: Int): Int {
     val resultFee = if (amount * fee > minFee) {
-        amount * fee
-    }else {
+        (amount * fee).toInt()
+    } else {
         minFee
     }
-//    println(
-//        """
-//        Сумма перевода: $amount
-//        Размер комиссии составит: $resultFee
-//        """.trimIndent()
-//    )
+    return resultFee
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,26 +1,74 @@
+import com.sun.tools.javac.Main
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class MainKtTest {
 
+
     @Test
-    fun main() {
-//        println("Введи сумму перевода")
+    fun onlyForTest() {
+        val IntForTest = 5
+        val StringForTest = "Like"
+        val forTest = onlyForTest(IntForTest, StringForTest)
+        assertEquals("10 LikeLike", forTest)
+    }
+
+    @Test
+    fun onlyForTestAnotherResult() {
+        val IntForTest = 1
+        val StringForTest = "Like"
+        val forTest = onlyForTest(IntForTest, StringForTest)
+        assertEquals("LikeLike", forTest)
+    }
+
+    @Test
+    fun calculateFee() {
+
         val amount = 100
         val fee = 0.0075
         val minFee = 35
-        val resultFee = if (100 * 0.0075 > 35){
-            100 * 0.0075
-        } else {
-            35
-        }
-//        println(
-//            """
-//        Сумма перевода: $amount
-//        Размер комиссии составит: $resultFee
-//        """.trimIndent()
-//        )
-        assertEquals(35, resultFee)
+
+        val resultFee = if (amount * fee > minFee) amount * fee.toInt() else minFee
+
+        val result = calculateFee(amount, fee, minFee)
+        assertEquals(35, result)
+    }
+    @Test
+    fun calculateFeeAnotherResult() {
+
+        val amount = 10000
+        val fee = 0.0075
+        val minFee = 35
+
+        val resultFee = if (amount * fee > minFee) amount * fee.toInt() else minFee
+
+        val result = calculateFee(amount, fee, minFee)
+        assertEquals(75, result)
+    }
+
+    @Test
+    fun testMain() {
+        val amount = 100
+        val fee = 0.0075
+        val minFee = 35
+
+        val expectedOutput = """
+            Сумма перевода: $amount
+            Размер комиссии составит: ${calculateFee(amount, fee, minFee)}
+        """.trimIndent()
+
+        val outputStreamCaptor = SystemOutCaptor()
+        System.setOut(PrintStream(outputStreamCaptor))
+
+        main()
+
+        assertEquals(expectedOutput, outputStreamCaptor.output.trim())
+    }
+
+    private class SystemOutCaptor : ByteArrayOutputStream() {
+        val output: String
+            get() = toString()
     }
 }
